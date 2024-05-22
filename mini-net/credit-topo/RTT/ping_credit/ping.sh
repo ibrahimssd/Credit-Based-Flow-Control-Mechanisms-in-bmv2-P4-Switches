f4a7c443f8eee1305f7453fe3fd629521847e86e
Ibrahim Siddig <38038497+ibrahimssd@users.noreply.github.com>
2024-05-22 19:33:26 +0200

    Add files via upload

---
+++ mini-net/credit-topo/RTT/ping_credit/ping.sh
@@ -0,0 +1,9 @@
+
+
+
+
+
+# ping -w 20  h2 > ping1_qrate10_qdepth100_credits_RTT60.txt
+# ping -w 20  h2 > ping2_qrate10_qdepth100_credits_RTT60.txt
+# ping -w 20  h2 > ping3_qrate10_qdepth100_credits_RTT60.txt
+ping -w 20  h2 > ping4_qrate10_qdepth100_credits_RTT60.txt
